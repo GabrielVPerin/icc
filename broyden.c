be4c8260f0d32363be8b507f7a5e6febc167504f
Gabriel Perin <gvp24@inf.ufpr.br>
2026-05-29 23:53:44 -0300

    consertado evaluador das derivadas parciais

--- broyden.c
+++ broyden.c
@@ -12,16 +12,50 @@ double equacoes_broyden(size_t linha, size_t n, double *parametros_atuais)
         return (-2 * pow(parametros_atuais[linha], 2) + 3 * parametros_atuais[linha] - parametros_atuais[linha-1] - 2 * parametros_atuais[linha+1] + 1);
 }
 
-double derivadas_broyden(size_t linha, size_t variavel, size_t n, double *parametros_atuais)
+double derivadas_broyden(size_t linha, size_t variavelDerivada, size_t n, double *parametros_atuais)
 {
-    if(linha == variavel)
+    if(linha == variavelDerivada)
         return (-4 * parametros_atuais[linha] + 3);
-    else if(linha == variavel - 1)
+    else if(linha != (n - 1) && linha == (variavelDerivada - 1))
         return -2;
-    else if(linha == variavel +1)
+    else if(linha != 0 && linha == (variavelDerivada + 1))
         return -1;
-    else
-        return 0;
 
-    
+    return 0;
+}
+
+int main()
+{
+    size_t n = 10;
+    // double x0 = 2;
+
+    double *fx = malloc(n * sizeof(double));
+    double *x = malloc(n * sizeof(double));
+    for(size_t i = 0; i < n; i++) {
+        x[i] = i;
+    }
+
+    double **jacobiana = malloc(n * sizeof(double *));
+    for(size_t i = 0; i < n; i++) {
+        jacobiana[i] = malloc(n * sizeof(double));
+    }
+
+    for(size_t i = 0; i < n; i++) {
+        for(size_t j = 0; j < n; j++) {
+            jacobiana[i][j] = derivadas_broyden(i, j, n, x);
+            printf("%.2f ", jacobiana[i][j]);
+        }
+        puts("");
+    }
+
+    puts("");
+
+    for(size_t i = 0; i < n; i++) {
+        fx[i] = equacoes_broyden(i, n, x);
+        printf("%.2f ", fx[i]);
+    }
+
+    puts("");
+
+    return EXIT_SUCCESS;
 }
